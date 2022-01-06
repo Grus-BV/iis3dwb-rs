f5f9f0b697a02c68e3be95ec38953b4d039a39e7
Mehmet Ali Anil <mehmet@grusbv.com>
2022-01-06 22:38:30 +0100

    again removing int set

--- src/lib.rs
+++ src/lib.rs
@@ -106,7 +106,7 @@ where
         }
         // set_range
         iis3dwb.set_range(iis3dwb.range);
-        iis3dwb.set_interrupt_1(iis3dwb.interrupt1);
+        //iis3dwb.set_interrupt_1(iis3dwb.interrupt1);
         Ok(iis3dwb)
     }  
 
